5a3088de852f5b628bca877dfd77b69a406b3121
Cheoroo <195822@jnu.ac.kr>
2023-11-04 21:49:41 +0900

    fix: 삭제된 파일은 안보이게끔 DTO 수정
     세차장 상세 정보 조회 (리뷰 제외)
     현재 시간 기준 예약 내역 조회
     최근 예약 내역 가져오기
     개별 매장 수정 - 기존 정보 불러오기
     매장 관리 - owner별, 세차장별
     사장님 홈

--- src/main/java/bdbe/bdbd/member/OwnerResponse.java
+++ src/main/java/bdbe/bdbd/member/OwnerResponse.java
@@ -154,7 +154,11 @@ public class OwnerResponse {
                 BayReservationDTO bayReservationDTO = new BayReservationDTO(bay, reservationList);
                 this.bays.add(bayReservationDTO);
             }
-            this.image = (file != null) ? new FileDTO(file) : null;
+            if (file != null && !file.isDeleted()) {
+                this.image = new FileDTO(file);
+            } else {
+                this.image = null;
+            }
         }
 
     }
@@ -261,7 +265,10 @@ public class OwnerResponse {
             this.carwashId = carwash.getId();
             this.monthlySales = monthlySales;
             this.monthlyReservations = monthlyReservations;
-            this.imageFiles = files.stream().map(FileDTO::new).collect(Collectors.toList());
+            this.imageFiles = files.stream()
+                    .filter(file -> !file.isDeleted())  // 삭제되지 않은 파일만 포함
+                    .map(FileDTO::new)
+                    .collect(Collectors.toList());
 
         }
     }
