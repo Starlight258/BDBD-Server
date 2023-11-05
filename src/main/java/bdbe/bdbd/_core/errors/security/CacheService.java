90e955e10d003604b6e82bc7f8bd04344a35ca98
Cheoroo <195822@jnu.ac.kr>
2023-11-05 23:51:15 +0900

    fix: 토큰이 로그아웃 이후 정상적으로 캐시에서 지워지도록 수정

--- src/main/java/bdbe/bdbd/_core/errors/security/CacheService.java
+++ src/main/java/bdbe/bdbd/_core/errors/security/CacheService.java
@@ -3,6 +3,9 @@ package bdbe.bdbd._core.errors.security;
 import bdbe.bdbd.member.MemberResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
@@ -12,6 +15,12 @@ import org.springframework.stereotype.Service;
 @CacheConfig(cacheNames = "jwtTokens")
 public class CacheService {
 
+    private final CacheManager cacheManager;
+
+    @Autowired
+    public CacheService(CacheManager cacheManager) {
+        this.cacheManager = cacheManager;
+    }
     private static final Logger logger = LoggerFactory.getLogger(CacheService.class);
 
     @Cacheable(key = "#token", condition = "#token != null")
@@ -24,9 +33,18 @@ public class CacheService {
         logger.info("Token evicted from cache: {}", token);
     }
 
+    public boolean isTokenCached(String token) {
+        Cache cache = cacheManager.getCache("jwtTokens");
+        if (cache != null && cache.get(token) != null) {
+            return true;
+        }
+        return false;
+    }
+
     public MemberResponse.LogoutResponse logout(String token) {
         evictToken(token);
         MemberResponse.LogoutResponse response = new MemberResponse.LogoutResponse();
         response.setSuccess(true);
         return response;
-    }}
+    }
+}
