62744ccf99f4dc9cf2fdbeb2acd1e2a6f8a1fa5e
Starlight258 <audwl03071@gmail.com>
2023-11-06 17:44:54 +0900

    feat: 유효성 검증 및 예외처리

--- src/main/java/bdbe/bdbd/review/ReviewRequest.java
+++ src/main/java/bdbe/bdbd/review/ReviewRequest.java
@@ -7,6 +7,10 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 public class ReviewRequest {
@@ -14,10 +18,20 @@ public class ReviewRequest {
     @Setter
     @ToString
     public static class SaveDTO {
+        @NotNull(message = "세차장 ID는 필수입니다.")
         private Long carwashId;
+
+        @NotNull(message = "예약 ID는 필수입니다.")
         private Long reservationId;
-        private List<Long> keywordIdList;
+
+        private List<Long> keywordIdList; // 필수가 아님
+
+        @NotNull(message = "평점은 필수입니다.")
+        @DecimalMax(value = "5.0", message = "평점은 5점을 초과할 수 없습니다.")
         private double rate;
+
+        @Size(max = 100, message = "댓글은 100자 이내여야 합니다.")
+        @NotBlank(message = "댓글은 필수입니다.")
         private String comment;
 
 
