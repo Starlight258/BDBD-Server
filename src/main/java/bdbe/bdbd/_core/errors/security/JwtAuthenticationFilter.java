c4dfb7f8ccf75d287cc7de04ecead7a5b6c65a16
Starlight258 <78211281+Starlight258@users.noreply.github.com>
2023-11-07 22:15:03 +0900

    Revert "fix: 로그아웃 기능 정상 작동 하게끔 코드 수정"

--- src/main/java/bdbe/bdbd/_core/errors/security/JwtAuthenticationFilter.java
+++ src/main/java/bdbe/bdbd/_core/errors/security/JwtAuthenticationFilter.java
@@ -1,14 +1,11 @@
 package bdbe.bdbd._core.errors.security;
 
-import bdbe.bdbd._core.errors.exception.UnAuthorizedError;
 import bdbe.bdbd.member.Member;
 import bdbe.bdbd.member.MemberRole;
 import com.auth0.jwt.exceptions.SignatureVerificationException;
 import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -21,46 +18,28 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-import static bdbe.bdbd._core.errors.security.JWTProvider.TOKEN_PREFIX;
-
 @Slf4j
 public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
 
-    private final CacheService cacheService;
-    private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
-
-    public JwtAuthenticationFilter(AuthenticationManager authenticationManager, CacheService cacheService) {
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
         super(authenticationManager);
-        this.cacheService = cacheService;
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-        String header = request.getHeader(JWTProvider.HEADER);
-        String requestURI = request.getRequestURI();
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String jwt = request.getHeader(JWTProvider.HEADER);
 
-        if ("/api/user/logout".equals(requestURI)) {
+        if (jwt == null) {
             chain.doFilter(request, response);
             return;
         }
-
-        if (header == null || !header.startsWith(TOKEN_PREFIX)) {
-            chain.doFilter(request, response);
-            return;
-        }
-
-        String jwt = header.replace(TOKEN_PREFIX, "");
 
         try {
             DecodedJWT decodedJWT = JWTProvider.verify(jwt);
-            if (!cacheService.isTokenCached(jwt)) {
-                logger.warn("Attempt to use a token that is not cached or has been evicted: Bearer {}", jwt);
-                throw new UnAuthorizedError("Token is not cached");
-            }
-
             Long id = decodedJWT.getClaim("id").asLong();
             String role = decodedJWT.getClaim("role").asString();
+            log.info("role: {}", role);
+
             MemberRole roleEnum = MemberRole.valueOf(role);
             Member member = Member.builder().id(id).role(roleEnum).build();
 
@@ -79,15 +58,11 @@ public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
         } catch (TokenExpiredException tee) {
             sendErrorResponse(response, HttpServletResponse.SC_UNAUTHORIZED, "JWT has expired");
             return;
-        } catch (UnAuthorizedError uae) {
-            sendErrorResponse(response, HttpServletResponse.SC_UNAUTHORIZED, uae.getMessage());  // UnAuthorizedError 예외 처리
-            return;
         } catch (Exception e) {
             sendErrorResponse(response, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "An unexpected error occurred");
             return;
         }
         chain.doFilter(request, response);
-
     }
     private void sendErrorResponse(HttpServletResponse response, int status, String message) {
         response.setStatus(status);
