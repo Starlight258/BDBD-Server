e53b7b7f1f1d46d88b105773c28db1bb83d122f9
2Using <yjlee194852@gmail.com>
2023-11-09 02:03:36 +0900

    delete: 사용하지 않는 코드 삭제

--- src/main/java/bdbe/bdbd/pay/PayResponse.java
+++ src/main/java/bdbe/bdbd/pay/PayResponse.java
@@ -40,7 +40,6 @@ public class PayResponse {
     public static class Amount {
         private Integer total;
         private Integer tax_free;
-        private Integer vat;
         private Integer point;
         private Integer discount;
         private Integer green_deposit;
