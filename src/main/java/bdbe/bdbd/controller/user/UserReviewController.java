c0088007d18d7d3994cffe45a7cb0982a3a31d9d
Starlight258 <audwl03071@gmail.com>
2023-11-09 14:45:05 +0900

    refactor: 패키지 분리

--- src/main/java/bdbe/bdbd/controller/user/UserReviewController.java
+++ src/main/java/bdbe/bdbd/controller/user/UserReviewController.java
@@ -1,4 +1,4 @@
-package bdbe.bdbd.controller.review;
+package bdbe.bdbd.controller.user;
 
 import bdbe.bdbd._core.security.CustomUserDetails;
 import bdbe.bdbd._core.utils.ApiUtils;
@@ -15,32 +15,16 @@ import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api")
-public class ReviewRestController {
+@RequestMapping("/api/user")
+public class UserReviewController {
 
     private final ReviewService reviewService;
 
     // 리뷰 등록 기능
-    @PostMapping("/user/reviews")
+    @PostMapping("/reviews")
     public ResponseEntity<?> createReview (@RequestBody @Valid ReviewRequest.SaveDTO saveDTO, Errors errors, @AuthenticationPrincipal CustomUserDetails userDetails) {
         reviewService.createReview(saveDTO, userDetails.getMember());
         return ResponseEntity.ok(ApiUtils.success(null));
-    }
-
-    // 리뷰 조회 기능
-    @GetMapping("/open/carwashes/{carwashId}/reviews")
-    public ResponseEntity<?> getReviewsByCarwashId(@PathVariable("carwashId") Long carwashId) {
-        ReviewResponse.ReviewResponseDTO dto = reviewService.getReviewsByCarwashId(carwashId);
-
-        return ResponseEntity.ok(ApiUtils.success(dto));
-    }
-
-    // 리뷰 키워드 불러오기
-    @GetMapping("/open/reviews")
-    public ResponseEntity<?> getReviewKeyword() {
-        ReviewResponse.ReviewKeywordResponseDTO dto = reviewService.getReviewKeyword();
-
-        return ResponseEntity.ok(ApiUtils.success(dto));
     }
 
 }
