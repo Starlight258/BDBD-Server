9df3252d4f26f99cd59cd2c0bf03ce4af2739266
Cheoroo <195822@jnu.ac.kr>
2023-11-10 15:59:55 +0900

    fix: 에러 메시지가 정상적으로 반영되게끔 코드 수정 및 지정된 에러코드로 반환되게끔 에러 종류 수정

--- src/main/java/bdbe/bdbd/service/reservation/ReservationService.java
+++ src/main/java/bdbe/bdbd/service/reservation/ReservationService.java
@@ -34,7 +34,6 @@ import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.NoSuchElementException;
 
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
