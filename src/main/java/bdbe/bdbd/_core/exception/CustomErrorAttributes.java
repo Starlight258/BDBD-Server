a1fb45f514e53570b23bfee0512edf7edd990322
Starlight258 <audwl03071@gmail.com>
2023-11-10 16:06:20 +0900

    feat: 에러 코드 형식 일관화

--- src/main/java/bdbe/bdbd/_core/exception/CustomErrorAttributes.java
+++ src/main/java/bdbe/bdbd/_core/exception/CustomErrorAttributes.java
@@ -1,5 +1,6 @@
 package bdbe.bdbd._core.exception;
 
+import org.apache.http.HttpStatus;
 import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
 import org.springframework.stereotype.Component;
@@ -20,17 +21,15 @@ public class CustomErrorAttributes extends DefaultErrorAttributes {
         boolean errorOccurred = !defaultErrorAttributes.get("status").equals(200);
         int status = (int) defaultErrorAttributes.get("status");
 
-        // 성공 여부를 설정합니다. 에러가 발생했으면 false, 아니면 true입니다.
         customErrorAttributes.put("success", !errorOccurred);
 
-        // 에러 발생시에만 'error' 키를 포함시키고, 'response'를 null로 설정합니다.
         if (errorOccurred) {
             Map<String, Object> errorDetails = new LinkedHashMap<>();
 
-            // 404 에러일 경우 메시지를 "Not Found"로 설정합니다.
-            String errorMessage = (status == 404) ? "Not Found" : (String) defaultErrorAttributes.get("message");
+            String errorMessage = (status == HttpStatus.SC_NOT_FOUND) ? NotFoundError.ErrorCode.RESOURCE_NOT_FOUND.getMessage() : (String) defaultErrorAttributes.get("message");
+            errorDetails.put("status", String.valueOf(status));
+            errorDetails.put("code", String.valueOf(NotFoundError.ErrorCode.RESOURCE_NOT_FOUND.getCode()));
             errorDetails.put("message", errorMessage);
-            errorDetails.put("status", status);
 
             customErrorAttributes.put("response", null); // 에러 발생 시 response는 null
             customErrorAttributes.put("error", errorDetails);
