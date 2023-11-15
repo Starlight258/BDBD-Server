8ed32b999561cbae54d080fad73fa8df5a772c9e
Starlight258 <audwl03071@gmail.com>
2023-11-15 17:26:48 +0900

    fix: 버그 수정

--- src/main/java/bdbe/bdbd/repository/bay/BayJPARepository.java
+++ src/main/java/bdbe/bdbd/repository/bay/BayJPARepository.java
@@ -12,7 +12,7 @@ public interface BayJPARepository extends JpaRepository<Bay, Long> {
 
     @Query("SELECT b.id FROM Bay b WHERE b.carwash.id = :carwashId")
     List<Long> findIdsByCarwashId(@Param("carwashId") Long carwashId);
-
+    List<Bay> findByCarwashId(Long carwashId);
     @Query("SELECT b FROM Bay b WHERE b.carwash.id = :carwashId AND b.status = 1")
     List<Bay> findByCarwashIdAndStatus(Long carwashId);
 
